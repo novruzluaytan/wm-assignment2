cecf48493a707fe30437e7de647798904d4f7522
Aytan Novruzlu <aytannovruzlu@Aytans-MacBook-Air-4.local>
2023-11-28 11:45:58 +0400

    fetch the data from dummyjson.com and sytle fetched data

---
+++ app.js
@@ -0,0 +1,27 @@
+var container = document.querySelector(".container");
+
+window.onload = getData()
+
+function getData() {
+    axios.get("https://dummyjson.com/products").then((res) => {
+        res.data.products.forEach((el, index) => {
+            container.innerHTML +=
+            `<div class="productContainer">
+                <div class="top">
+                    <img src="${el.thumbnail}">
+                </div>
+                <div class="mid">
+                    <div class="title">${el.title}</div>
+                    <div class="price">Price: $${el.price}</div>
+                    <div class="discount">Discount Percentage: %${el.discountPercentage}</div>
+                    <div class="stock">Stock: ${el.stock}</div>
+                    <div class="rating">Rating: ${el.rating}/5</div>
+                </div>
+          </div>`;
+        });
+    });
+}
+
+
+
+
