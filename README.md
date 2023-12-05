b56f63e64f51899590e879bb20b7ee84e1e32d84
Aytan Novruzlu <aytannovruzlu@Aytans-MacBook-Air-4.local>
2023-12-05 21:57:09 +0400

    added pagination navigation to the page, made 10 products visible on one page. Changed few JS and HTML codes

--- README.md
+++ README.md
@@ -1,23 +1,84 @@
 # wm-assignment2
 
-1. API integration 
-	A)  Use JavaScript to fetch data from dummyjson.com about products from / products endpoint.
-	B)  Handle any potential errors during the fetch operation.
+Assignment2 - Web & Mobile 1 README
+Welcome to the Assignment2 - Web & Mobile 1! This README file will guide you through the setup and usage of the application. Please follow the instructions carefully to ensure a smooth experience.
+
+Table of Contents
+API Integration
+Data Display (1)
+Data Display (2)
+Data Display (3)
+Apply Pagination
+API Integration
+A) Use JavaScript to fetch data
+To integrate the API and fetch product data from dummyjson.com, follow these steps:
+
+Open the index.html file in your preferred code editor.
+
+Locate the JavaScript section (usually between <script> tags).
+
+Before the <script> tags, create another <script> tag and include src="https://unpkg.com/axios/dist/axios.min.js" as a source.
+
+Implement the fetch operation using JavaScript. Example:
+
+Create a function, add the link to the beginning of the code. You can write whatever you 
+function getData() {
+    axios.get("https://dummyjson.com/products").then((res) => {
+        container.innerHTML = "";
+        if (res.data && Array.isArray(res.data.products)) {
+            res.data.products.forEach((el, index) => {
+                container.innerHTML +=
+                    `
+					//write the html part here
+
+					`;
+            });
+
+        } else {
+            console.error("Invalid response or missing product");
+        }
+    })
+        .catch((error) => {
+            console.error("Error:", error.message);
+        });
+}
 	
-2. Data display (1) 
-	A) Display the data on the home web page. Each product should include the title, price, discount, category, and stock as well as the thumbnail.
-	B) Ensure the display is well-formatted and readable.
-	
-3. Data display (2) 
-	A) When a product is clicked a new product info page is opened with the detailed information as well as the gallery of the product.
-	
-4. Data display (3)
-	A) On the home page, provide the following features
-		i. Search for a keyword (it can be a part of title, desc, or even category) (15pts)
-		ii. Filter based on the given category (all categories are to be provided as a select box and choosing any category from the list filters the list of all the products) (15 pts)
-	B) Use API documentation to explore available ones and their usage.
-	
-5. Apply pagination 
-A) If the result of any response holds more than 10 objects, show only 10products. Implement pagination navigation at the bottom of the list.
+B) Handle Potential Errors
+Make sure to handle potential errors during the fetch operation. This could include network issues, server errors, or unexpected API responses. Update the catch block in the fetch operation to provide appropriate error handling.
+
+Data Display (1)
+A) Display Data on Home Page
+Display product information on the home web page. Ensure that each product includes the title, price, discount, category, stock, and thumbnail. Modify the HTML structure to incorporate this information.
+
+B) Format Display
+Ensure the displayed data is well-formatted and readable. Utilize CSS styles to enhance the visual presentation of the product information.
+
+Data Display (2)
+A) Product Info Page
+Implement a new product info page that opens when a product is clicked. This page should display detailed information about the selected product, along with a gallery of images.
+
+Data Display (3)
+A) Search and Filter Features
+On the home page, provide the following features:
+
+i. Search for a Keyword
+Implement a search functionality that allows users to search for a keyword (part of title, description, or category). Update the JavaScript code to handle search queries.
+
+ii. Filter Based on Category
+Create a select box containing all available categories. Choosing any category from the list should filter the displayed products accordingly.
+
+B) API Documentation
+Refer to the API documentation to explore available endpoints and their usage. Update the fetch operation and implement necessary changes based on the API documentation.
+
+Apply Pagination
+A) Implement Pagination
+If the API response contains more than 10 products, display only the first 10 and implement pagination navigation at the bottom of the product list. Allow users to navigate through the pages and fetch the corresponding data.
+
+
+
+
+
+
+
 
 
--- app.js
+++ app.js
@@ -1,47 +1,58 @@
-var container = document.querySelector(".container");
+var container = document.querySelector(".containeer");
 var navbar = document.querySelector(".navbar")
 var categoryDropDown = document.getElementById("category");
 var searchItem = document.getElementById("myInput");
-var infoPage = document.querySelector(".infoPage")
+var infoPage = document.querySelector(".infoPage");
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
+
+var requestUrl = "https://dummyjson.com/products"
+
+const skip = 10
+const itemsPerPage = 10
+var page = 0
 
 window.onload = getData()
 window.onload = fetchCategories()
 
 
 function getData() {
-    axios.get("https://dummyjson.com/products").then((res) => {
+    var request = requestUrl + "?limit=" + itemsPerPage + "&skip=" + (skip*page)
+    axios.get(request).then((res) => {
         container.innerHTML = "";
         if (res.data && Array.isArray(res.data.products)) {
             res.data.products.forEach((el, index) => {
                 container.innerHTML +=
-                    `<div class="productContainer" onClick="onProductClick(${el.id})">
-                <div class="top">
-                    <img src="${el.thumbnail}">
-                </div>
-                <div class="mid">
-                    <div class="title">${el.title}</div>
-                    <div class="discount">Discount Percentage: %${el.discountPercentage}</div>
-                    <div class="stock">Stock: ${el.stock}</div>
-                    <div class="rating">Rating: ${el.rating}/5</div>
-                    <div class="price">$${el.price}</div>
-                    <div class="category">${el.category}</div>
-                </div>
-            </div>`;
+                        `<div class="productContainer" onClick="onProductClick(${el.id})">
+                    <div class="top">
+                        <img src="${el.thumbnail}">
+                    </div>
+                    <div class="mid">
+                        <div class="title">${el.title}</div>
+                        <div class="discount">Discount Percentage: %${el.discountPercentage}</div>
+                        <div class="stock">Stock: ${el.stock}</div>
+                        <div class="rating">Rating: ${el.rating}/5</div>
+                        <div class="price">$${el.price}</div>
+                        <div class="category">${el.category}</div>
+                    </div>
+                </div>`;
             });
+
+            if(page == 0) {
+                populatePagination(res.data.total)
+            }
 
         } else {
             console.error("Invalid response or missing product");
         }
     })
-        .catch((error) => {
-            console.error("Error:", error.message);
-        });
+    .catch((error) => {
+        console.error("Error:", error.message);
+    });
 }
-
 
 function fetchCategories() {
     axios.get("https://dummyjson.com/products/categories").then((res) => {
-
 
         let option = document.createElement("option");
         option.setAttribute('value', "All");
@@ -52,7 +63,6 @@ function fetchCategories() {
 
         categoryDropDown.appendChild(option);
 
-
         res.data.forEach((e, index) => {
 
             let option = document.createElement("option");
@@ -62,70 +72,47 @@ function fetchCategories() {
             option.appendChild(optionText);
 
             categoryDropDown.appendChild(option);
-
         });
     });
 };
 
 function onCategorySelect() {
+    searchItem.value = ""
+    page = 0
     var selectedOption = categoryDropDown.options[categoryDropDown.selectedIndex].text;
     if (selectedOption == "All") {
-        getData()
+        requestUrl = "https://dummyjson.com/products"
     }
     else {
-        axios.get("https://dummyjson.com/products/category/" + selectedOption).then((res) => {
-            container.innerHTML = ""
-            res.data.products.forEach((el, index) => {
-                container.innerHTML +=
-                    `<div class="productContainer"  onClick="onProductClick(${el.id}))">
-                    <div class="top">
-                        <img src="${el.thumbnail}">
-                    </div>
-                    <div class="mid">
-                        <div class="title">${el.title}</div>
-                        <div class="discount">Discount Percentage: %${el.discountPercentage}</div>
-                        <div class="stock">Stock: ${el.stock}</div>
-                        <div class="rating">Rating: ${el.rating}/5</div>
-                        <div class="price">$${el.price}</div>
-                    </div>
-                </div>`;
-            });
-        });
+        requestUrl = "https://dummyjson.com/products/category/" + selectedOption
     }
-
+    getData()
 }
-
 
 function onSearch() {
+    page = 0
+    categoryDropDown.selectedIndex = "0"
     var searchQuery = searchItem.value
-    axios.get("https://dummyjson.com/products/search?q=" + searchQuery).then((res) => {
-        container.innerHTML = ""
-        res.data.products.forEach((el, index) => {
-            container.innerHTML +=
-                `<div class="productContainer" onClick="onProductClick(${el.id})">
-                <div class="top">
-                    <img src="${el.thumbnail}">
-                </div>
-                <div class="mid">
-                    <div class="title">${el.title}</div>
-                    <div class="discount">Discount Percentage: %${el.discountPercentage}</div>
-                    <div class="stock">Stock: ${el.stock}</div>
-                    <div class="rating">Rating: ${el.rating}/5</div>
-                    <div class="price">$${el.price}</div>
-                </div>
-            </div>`;
-        });
-    });
+    requestUrl = "https://dummyjson.com/products/search?q=" + searchQuery + "&"
+    getData()
 }
-
 
 function onProductClick(id) {
     localStorage.setItem("id", id)
     location.href = "./infoPage.html";
 }
 
+function populatePagination(total) {
+    var pageNumber = total/itemsPerPage
+    var ul = document.getElementById("pagination")
+   ul.innerHTML = '';
+    for (let i = 0; i < pageNumber; i++) { 
+        ul.innerHTML += ` <li><a onClick="onPageClick(${i})" href="#">${i+1}</a></li>`
+ 
+    }
+}
 
-
-
-
-
+function onPageClick(pageNumber) {
+    page = pageNumber
+    getData()
+}
